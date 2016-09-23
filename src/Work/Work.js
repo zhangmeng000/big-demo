9d27f409f848e6328f07a1c21b3794bfc09fc729
zhangmeng000 <1550685677@qq.com>
2016-09-23 09:08:53 +0800

    ls

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,12 +1,28 @@
 import React from 'react'
-
+import Bg from '../images/02.jpg'
 class Work extends React.Component {
   render () {
+    let styles = {
+      root:{
+
+      },
+      img:{
+        width:'100%',
+        height:'auto',
+        margin:'10px auto',
+        display:'block',
+        borderRadius:'5px'
+      }
+    }
     return(
-      <div className="cover">
-        <p>工作经历</p>
-        <p>15.5-16.7</p>
-        <span className = 'blogsp'>团队合作，前端制作，维护网页，与后台积极沟通</span>
+      <div className="container-fluid">
+        <div className="row">
+            <div className="col-sm-6 col-md-4 work-nav"><img style = {styles.img} src = {Bg} /><a href = '#' className = 'nav-zp'><h3>作品1</h3><p>2015.8.10</p></a></div>
+            <div className="col-sm-6 col-md-4 work-nav"><img style = {styles.img} src = {Bg} /><a href = '#' className = 'nav-zp'><h3>作品2</h3><p>2015.9.12</p></a></div>
+            <div className="col-sm-6 col-md-4 work-nav"><img style = {styles.img} src = {Bg} /><a href = '#' className = 'nav-zp'><h3>作品3</h3><p>2016.3.12</p></a></div>
+            <div className="col-sm-6 col-md-4 work-nav"><img style = {styles.img} src = {Bg} /><a href = '#' className = 'nav-zp'><h3>作品4</h3><p>2016.5.15</p></a></div>
+            <div className="col-sm-6 col-md-4 work-nav"><img style = {styles.img} src = {Bg} /><a href = '#' className = 'nav-zp'><h3>作品5</h3><p>2016.9.19</p></a></div>
+        </div>
       </div>
     )
   }
--- src/component/Navheader.js
+++ src/component/Navheader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router'
 class Navheader extends React.Component {
   handleBack(){
       this.context.router.goBack();
@@ -9,7 +9,7 @@ class Navheader extends React.Component {
         <div className = 'nav-header'>
           <p onClick={this.handleBack.bind(this)}><span className="glyphicon glyphicon-arrow-left"></span>Back</p>
           zhangmeng@{this.props.title}
-          <p><span className="glyphicon glyphicon-th"></span></p>
+          <p><Link to = '/'><span className="glyphicon glyphicon-th"></span></Link></p>
         </div>
       )
   }
